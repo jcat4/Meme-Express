508ff6d0a65fddd9c95142241b801ad959360050
jcatfour <jcatfour@gmail.com>
2017-07-05 17:10:04 -0400

    revised json parsing

--- Start.py
+++ Start.py
@@ -6,16 +6,14 @@ import json
 fileDir = "/home/cabox/workspace/Meme Express Info.json"
 with open(fileDir) as secretFile:
   data = json.load(secretFile)
-  fbData = data["facebook"]
-  driveData = data["drive"]
 
 # initiate
 pageAccessor = PageAccessor(\
-  fbData["token"], \
-  fbData["page_id"])
+  data["facebook"]["token"], \
+  data["facebook"]["page_id"])
 driveAccessor = DriveAccessor(\
-  driveData["test_folder_id"], \
-  driveData["test_backup_folder_id"])
+  data["drive"]["test_folder_id"], \
+  data["drive"]["test_backup_folder_id"])
 
 # Grab and download meme, if any
 driveAccessor.downloadMeme()
